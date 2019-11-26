b87e17419d5da36f0f7fd1195467b0b60fb33776
liudoubletian <52096751+liudoubletian@users.noreply.github.com>
2019-11-26 18:55:54 +0800

    Update simulate_dm.R

--- R/simulate_dm.R
+++ R/simulate_dm.R
@@ -11,13 +11,13 @@
 #' @param case_theta the over-dispersion parameter
 #' @return dtm_table the otu table
 #' @export
-simulation_dm=function(p,seed, N,control_pi, case_pi,control_theta,case_theta){
+simulation_dm <- function(p,seed, N,control_pi, case_pi,control_theta,case_theta){
   library(dirmult)
-  ntree_table = matrix(NA, 2*N, p)
+  ntree_table <- matrix(NA, 2*N, p)
   for(i in 1:N){
   set.seed(i*seed)
-  parent1 = round(runif(1, 5000, 50000))
-  parent2 = round(runif(1, 5000, 50000))
+  parent1 <- round(runif(1, 5000, 50000))
+  parent2 <- round(runif(1, 5000, 50000))
   set.seed(i*seed)
   ntree_table[i, ] <- simPop(J=1, K=p, n=parent1, pi = control_pi, theta = control_theta)$data
   set.seed(i*(seed+1))
