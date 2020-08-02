0adb2eef9c9f3a195f2a9964ed3abc8d3bbb85e7
liudoubletian <52096751+liudoubletian@users.noreply.github.com>
2020-08-02 22:35:43 +0800

    Update inter_func.R

--- R/inter_func.R
+++ R/inter_func.R
@@ -1,7 +1,7 @@
 #' @export
 #### estimate the parameter alpha on each internal node and conduct normalization####
 inter_func=function(g,tree,tree_table,group,test.method){
-  library(MGLMreg)
+  library(MGLM)
   sample.s <- nrow(tree_table)
   case <- which(group == 0)
   con <- which(group == 1)
